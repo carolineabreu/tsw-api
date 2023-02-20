ba6810a618cf73daeaacb94f0de831df43347d12
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2023-02-20 16:27:20 -0300

    :sparkles: feat: create comment route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import * as dotenv from "dotenv";
 import express from "express";
+import { commentRouter } from "./routes/comment.routes.js";
 import { countryRouter } from "./routes/country.routes.js";
 import { reviewRouter } from "./routes/review.routes.js";
 import { userRouter } from "./routes/user.routes.js";
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/country", countryRouter);
 app.use("/review", reviewRouter);
+app.use("/comment", commentRouter);
 
 app.listen(Number(process.env.PORT), () => {
   console.log(`Server up and running at port ${process.env.PORT}`);
---
+++ routes/comment.routes.js
@@ -0,0 +1,122 @@
+import { PrismaClient } from '@prisma/client';
+import express from 'express';
+import attachCurrentUser from "../middlewares/attachCurrentUser.js";
+import isAuth from '../middlewares/isAuth.js';
+
+const commentRouter = express.Router();
+const prisma = new PrismaClient();
+
+commentRouter.post(
+  "/:reviewId/new",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const { reviewId } = req.params;
+      const loggedInUser = req.currentUser;
+
+      const comment = await prisma.comment.create({
+        data: {
+          ...req.body,
+          authorId: loggedInUser.id,
+          reviewId: reviewId
+        }
+      });
+
+      return res.status(201).json(comment);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  });
+
+commentRouter.get("/all", async (req, res) => {
+  try {
+    const comments = await prisma.comment.findMany({
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true
+          }
+        },
+        review: true,
+      }
+    });
+
+    return res.status(200).json(comments);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+commentRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const comment = await prisma.comment.findUnique({
+      where: {
+        id: id
+      },
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true
+          }
+        },
+        review: true
+      }
+    });
+
+    return res.status(200).json(comment);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+commentRouter.get("/byReview/:id", async (req, res) => {
+  try {
+    const commentsByReview = await prisma.comment.findMany({
+      where: {
+        review: {
+          is: {
+            id: req.params.id
+          }
+        }
+      }
+    });
+
+    if (!commentsByReview.length) {
+      return res.json({ message: "This review don't have any comments yet" });
+    }
+
+    return res.status(200).json(commentsByReview);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+});
+
+commentRouter.delete(
+  "/delete/:id",
+  isAuth,
+  attachCurrentUser,
+  async (req, res) => {
+    try {
+      const deleteComment = await prisma.comment.delete({
+        where: {
+          id: req.params.id
+        }
+      });
+
+      return res.status(200).json(deleteComment);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  });
+
+
+export { commentRouter };
